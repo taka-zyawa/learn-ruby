5e0f56394cf056b867d9cd01eb85041b67a33520
taka.hazawa <taka.hazawa@gmail.com>
2019-06-17 00:41:19 +0900

    多値の返却と、delete_if を使ってみた

--- lib/array.rb
+++ lib/array.rb
@@ -1,8 +1,23 @@
-numbers = [1, 2, 3, 4]
+puts
+puts '----- ループ -----'
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 sum = 0
 
 numbers.each do |n|
   sum+= n
 end
-
 puts sum
+
+puts
+puts '----- 多値返却 -----'
+def ret_array
+  %w(aだよ bですぞ)
+end
+
+a, b = ret_array
+puts a, b
+
+puts
+puts '----- delete_if -----'
+numbers.delete_if(&:odd?)
+puts(numbers)

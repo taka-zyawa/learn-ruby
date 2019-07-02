af1ac509240c8a5c37494da1cbf8c48409d84f2b
taka.hazawa <taka.hazawa@gmail.com>
2019-07-02 22:59:31 +0900

    配列でブロックを使う処理をいくつか写経

--- lib/array.rb
+++ lib/array.rb
@@ -4,18 +4,47 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 sum = 0
 
 numbers.each do |n|
-  sum+= n
+  sum += n
 end
 puts sum
 
 puts
 puts '----- 多値返却 -----'
+
 def ret_array
-  %w(aだよ bですぞ)
+  ["aだよ" "bですぞ"]
 end
 
 a, b = ret_array
 puts a, b
+
+puts
+puts '----- map / collect -----'
+c = numbers.map do |n|
+  if n.even?
+    n * 10
+  else
+    n
+  end
+end
+puts c
+
+puts
+puts '----- select / find_all / reject -----'
+puts numbers.select(&:even?)
+puts
+puts numbers.reject(&:even?)
+
+puts
+puts '----- find / detect -----'
+puts numbers.detect(&:even?)
+
+puts
+puts '----- reduce / inject -----'
+# puts numbers.reduce(0) { |result, n|
+#   result + n
+# }
+puts numbers.reduce(0, &:+)
 
 puts
 puts '----- delete_if -----'
